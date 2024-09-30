7e2e9186bb9a9e1bd2ae1e951c596f775f867885
Ekaterina Dul <ekaterina_dul@epam.com>
2024-09-30 21:54:20 +0400

    Modified sample of producer

--- producer/producer.py
+++ producer/producer.py
@@ -1,15 +1,19 @@
 import asyncio
 import os
+import json
 from dotenv import load_dotenv
 
 from azure.eventhub import EventData
 from azure.eventhub.aio import EventHubProducerClient
 
-
 load_dotenv("../.env")
+
+# LOADING EVENT HUB CONNECTION DATA
 EVENT_HUB_CONNECTION_STR = os.getenv("EVENT_HUB_CONNECTION_STR")
 EVENT_HUB_NAME = os.getenv("EVENT_HUB_NAME")
 
+
+# https://learn.microsoft.com/en-us/azure/event-hubs/event-hubs-python-get-started-send?tabs=connection-string%2Croles-azure-portal
 
 async def run():
     # Create a producer client to send messages to the event hub.
@@ -24,9 +28,13 @@ async def run():
 
         # Add events to the batch.
         # Batch size default: 100.
-        event_data_batch.add(EventData("First event "))
-        event_data_batch.add(EventData("Second event"))
-        event_data_batch.add(EventData("Third event"))
+        for i in range(50):
+            message = {
+                "id": i,
+                "message": f"{i} times hello",
+                "sender": "senior data engineer"
+            }
+            event_data_batch.add(EventData(json.dumps(message)))
 
         # Send the batch of events to the event hub.
         await producer.send_batch(event_data_batch)
