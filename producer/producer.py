e5f3e9eeadad5ae447b258146576a6adeee1321f
Ekaterina_Dul <ekaterina.dul@syngenta.com>
2024-10-01 13:54:16 +0400

    Ready-to-use producer

--- producer/producer.py
+++ producer/producer.py
@@ -1,8 +1,10 @@
 import asyncio
+import copy
 import os
 import json
 from dotenv import load_dotenv
-
+import tweepy as tw
+import jsonlines
 from azure.eventhub import EventData
 from azure.eventhub.aio import EventHubProducerClient
 
@@ -12,31 +14,72 @@ load_dotenv("../.env")
 EVENT_HUB_CONNECTION_STR = os.getenv("EVENT_HUB_CONNECTION_STR")
 EVENT_HUB_NAME = os.getenv("EVENT_HUB_NAME")
 
+# LOADING TWITTER TOKEN
+TWITTER_API_KEY = os.getenv("TWITTER_API_KEY")
+TWITTER_API_KEY_SECRET = os.getenv("TWITTER_API_KEY_SECRET")
 
-# https://learn.microsoft.com/en-us/azure/event-hubs/event-hubs-python-get-started-send?tabs=connection-string%2Croles-azure-portal
+TWITTER_ACCESS_TOKEN = os.getenv("TWITTER_ACCESS_TOKEN")
+TWITTER_ACCESS_TOKEN_SECRET = os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
+
+TWITTER_BEARER_TOKEN = os.getenv("TWITTER_BEARER_TOKEN")
+
+# MESSAGES BACKUP
+TWEETS_FILE_PATH = "../data/messages_backup.json"
+
+
+def save_message_backup(message_json):
+    with jsonlines.open(TWEETS_FILE_PATH, 'a') as json_writer:
+        json_writer.write(message_json)
+
+
+def generate_original_tweets(max_results: int = 10):
+    tw_client = tw.Client(
+        bearer_token=TWITTER_BEARER_TOKEN
+    )
+
+    query = '#elonmusk -is:retweet lang:en'
+    tweets = tw_client.search_recent_tweets(
+        query=query,
+        tweet_fields=['context_annotations', 'created_at'],
+        max_results=max_results
+    )
+    result = []
+
+    for message in tweets.data:
+        message_json = message._json
+        save_message_backup(message_json)
+        result.append(message_json)
+
+    return result
+
+
+def generate_dummy_tweets(max_results: int = 10):
+    tweet_structure = {}
+
+    with open("response_example.json", "r") as f:
+        tweet_structure = json.loads(f.read())
+
+    result = []
+    for i in range(max_results):
+        tweet_structure[
+            "text"] = f"Congratulations! You received {i}-th dummy tweet from dummy producers, and did not spend a cent on X API!"
+        result.append(json.dumps(tweet_structure))
+
+    return result
+
 
 async def run():
-    # Create a producer client to send messages to the event hub.
-    # Specify a connection string to your event hubs namespace and
-    # the event hub name.
     producer = EventHubProducerClient.from_connection_string(
-        conn_str=EVENT_HUB_CONNECTION_STR, eventhub_name=EVENT_HUB_NAME
+        conn_str=EVENT_HUB_CONNECTION_STR,
+        eventhub_name=EVENT_HUB_NAME
     )
     async with producer:
-        # Create a batch.
         event_data_batch = await producer.create_batch()
 
-        # Add events to the batch.
-        # Batch size default: 100.
-        for i in range(50):
-            message = {
-                "id": i,
-                "message": f"{i} times hello",
-                "sender": "senior data engineer"
-            }
-            event_data_batch.add(EventData(json.dumps(message)))
+        tweets = generate_dummy_tweets(max_results=10)
+        for message in tweets:
+            event_data_batch.add(EventData(message))
 
-        # Send the batch of events to the event hub.
         await producer.send_batch(event_data_batch)
 
 
